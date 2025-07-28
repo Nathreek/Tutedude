8b7de11fefae2d6f56d47e8a8ce5c36a4daf41bc
Nathreek <keerthanks007@gmail.com>
2025-07-28 10:42:44 +0530

    Create task2.py

---
+++ Assignment3/task2.py
@@ -0,0 +1,8 @@
+import math 
+n = int(input("Enter a number: "))
+sq_n = math.sqrt(n)
+log_n = math.log(n)
+sin_n = math.sin(n)
+print("Square root:", sq_n)
+print("Logarithm:", log_n)
+print("Sine:", sin_n)
