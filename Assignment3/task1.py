d453e456f1a85de2b11758fd138dd6fcfc8ceae2
Nathreek <keerthanks007@gmail.com>
2025-07-28 10:39:47 +0530

    Update task1.py

--- Assignment3/task1.py
+++ Assignment3/task1.py
@@ -1,1 +1,10 @@
+def factorial(n):
+    if n<0:
+        print("Enter positive numbers only")
+    elif n==0 or n==1 :
+        return 1
+    else:
+        return n*factorial(n-1)
 
+n = int(input("Enter a number: "))
+print("Factorial of",n ,"is", factorial(n))
