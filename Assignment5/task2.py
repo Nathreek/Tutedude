6c0ba8f0ff03b8f6aad4d5f3cbbfd5fd84618a48
Nathreek <keerthanks007@gmail.com>
2025-08-11 11:09:00 +0530

    Create task2.py

---
+++ Assignment5/task2.py
@@ -0,0 +1,6 @@
+l = list(range(1,11))
+print("Original list:", l)
+first5 = l[0:5]
+rev_first5 = list(reversed(first5))
+print("Extracted first 5 elements:", first5)
+print("Reversed extracted elements:", rev_first5)
